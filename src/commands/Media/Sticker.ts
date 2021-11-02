1946cc4637470e698fe55f2ca62ae6ec0a6e694e
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-11-02 09:35:28 +0530

    Update Sticker.ts

--- src/commands/Media/Sticker.ts
+++ src/commands/Media/Sticker.ts
@@ -78,8 +78,8 @@ export default class Command extends BaseCommand {
             })()
             return {
                 categories,
-                pack: pack[1] || '👾 For You ',
-                author: pack[2] || 'By Monarch+Kaoi 👾',
+                pack: pack[1] || 'Belongs to ',
+                author: pack[2] || 'Sukuna 🍜',
                 quality,
                 type: StickerTypes[
                     parsedArgs.flags.includes('--crop') || parsedArgs.flags.includes('--c')
