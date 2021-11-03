045e56e6323d8eb8b3a1ab5c7a9ee33d8f3cba05
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-11-03 21:30:05 +0530

    Update Kaoi.ts

--- src/commands/Bots/Kaoi.ts
+++ src/commands/Bots/Kaoi.ts
@@ -10,13 +10,29 @@ export default class Command extends BaseCommand {
             description: 'Displays the info',
             category: 'bots',
             usage: `${client.config.prefix}kaoi`,
-            baseXp: 200
+            baseXp: 10
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         return void M.reply(
-            `👾 *kaoi* 👾\n\n🍀 *Description:* Maintained Fork of WhatsApp Botto Void\n\n🌐 *URL:* https://github.com/PrajjwalDatir/Kaoi \n`
+            `━━━❪ SUKUNA ❫━━━
+
+📮 Q1: What kind of bot Sukuna is?
+📑 A: Sukuna is a simple anime themed What's app Bot Written in Typescript to help everyone in day to day life
+• ────── ✾ ────── •
+
+📮 Q2: How to add Sukuna in my group
+📑 A: Type !owner then send your grouplink to any of the moderators
+• ────── ✾ ────── •
+
+📮 Q3: Why doesn't the bot doesn't respond in my group?
+📑 A: During peak hours the bot may become slow due to traffic. If the bot doesn't respond for 12 hours or more, then the bot is probably banned from WhatsApp
+• ────── ✾ ────── •
+
+🪵 *Advice To Group Admin :*
+🐣 Make the bot admin of your group to access admin commands and better experience.
+• ────── ✾ ────── • \n`
         ).catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
     }
 }
