0b4f38dbc4ebbc098263b5f801e1bb3104b5c6e3
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-11-03 21:48:19 +0530

    Update Kaoi.ts

--- src/commands/Bots/Kaoi.ts
+++ src/commands/Bots/Kaoi.ts
@@ -6,10 +6,10 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'kaoi',
+            command: 'sukuna',
             description: 'Displays the info',
             category: 'bots',
-            usage: `${client.config.prefix}kaoi`,
+            usage: `${client.config.prefix}sukuna`,
             baseXp: 10
         })
     }
