80be4fe86e636547f75a6d0ee6eb5e2aa9c79a64
Lord Rakesh <89836389+lordrakesh@users.noreply.github.com>
2021-11-03 15:03:01 +0530

    Create do-you-mean.ts

---
+++ src/commands/General/do-you-mean.ts
@@ -0,0 +1,44 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '',
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/videos/Sukuna/sukunahelp.mp4'
+        ]
+        let sukuna = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: sukuna }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `火 Aah- you don't know how to type *${this.client.config.prefix}help*? \n` }
+        )
+    }
+}
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
