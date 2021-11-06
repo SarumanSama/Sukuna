de27ec259ba7554a404bdfc9bad0c7957d6c974a
00300 <90952986+well300@users.noreply.github.com>
2021-11-06 15:50:58 +0530

    Create Blowjob.ts

--- src/commands/Nsfw/Blowjob.ts
+++ src/commands/Nsfw/Blowjob.ts
@@ -22,7 +22,6 @@ export default class Command extends BaseCommand {
         // fetch result of https://api.waifu.pics/nsfw/blowjob from the API using axios
         const { data } = await axios.get('https://api.waifu.pics/nsfw/blowjob')
         const buffer = await request.buffer(data.url).catch((e) => {
-        const res = response as IaxiosResponse
         if (res.nsfw && !(await this.client.getGroupData(M.from)).nsfw)
             return void M.reply(
                 `Cannot Display NSFW content before enabling. Use ${this.client.config.prefix}activate nsfw to activate nsfw` )
