29853746e89ccc26400220ee7322c2b4d9463fae
SᴀʀᴜᴍᴀɴSᴀᴍᴀ <94067379+SarumanSama@users.noreply.github.com>
2021-11-14 22:41:46 +0530

    Create Hi.ts

---
+++ src/commands/General/Hi.ts
@@ -0,0 +1,28 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'hi',
+            description: 'Say hi to the bot.',
+            category: 'general',
+            usage: `${client.config.prefix}hi`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/videos/Sukuna/sukuna-hi.mp4'
+        ]
+        let kaoi = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: kaoi }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `火 ɠαɱზαɾҽ ɠαɱზαɾҽ 𝘴ꫀꪀρꪖ𝓲.. \n` }
+        )
+    }
+}
