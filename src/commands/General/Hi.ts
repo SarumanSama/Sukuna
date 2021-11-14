edd96793609c89eb433b8d9dbc49f67002035ef5
SᴀʀᴜᴍᴀɴSᴀᴍᴀ <94067379+SarumanSama@users.noreply.github.com>
2021-11-14 22:30:20 +0530

    well...

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -8,21 +8,21 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'hi',
-            description: 'Say hi to the bot.',
+            command: 'saruman',
+            description: 'Displays info Saruman Sama.',
             category: 'general',
-            usage: `${client.config.prefix}hi`
+            usage: `${client.config.prefix}saruman`
         })
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/videos/Sukuna/sukuna-hi.mp4'
+            './assets/videos/Sukuna/SarumanSama.mp4'
         ]
         let kaoi = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: kaoi }, MessageType.video, {
             mimetype: Mimetype.gif,
-            caption: `☕ *Follow* SarumanSama  *☕*
+            caption: `☕ *Follow* _SarumanSama_  *☕*
 
 🍃 *Github:* https://github.com/SarumanSama
 
