1e941fb4bb4a3d8379ba2e8bf3d27c3ecf715223
SᴀʀᴜᴍᴀɴSᴀᴍᴀ <94067379+SarumanSama@users.noreply.github.com>
2021-11-16 19:14:52 +0530

    Create Mods.ts

---
+++ src/commands/Bots/Mods.ts
@@ -0,0 +1,37 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'mods',
+            description: 'Shows mods of Sukuna.',
+            category: 'anime',
+            usage: `${client.config.prefix}mods`
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            './assets/videos/Sukuna/SukunaMods.mp4'
+        ]
+        let kaoi = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: kaoi }, MessageType.video, {quoted:M.WAMessage,
+            mimetype: Mimetype.gif,
+            caption: `*🪶Sᴜᴋᴜɴᴀ Mᴏᴅᴇʀᴀᴛᴏʀs 🪶*
+
+🦅→ ```SarumanSama```
+—> wa.me/94776264197
+
+🦅→ ```Lord Rakesh```
+—> wa.me/8473956301
+
+🦅→ ```Giga Chad```
+—> wa.me/666` }
+        )
+    }
+}
